e3e6a14be6d3b16fe8740d560d8b4a445fe828f4
yangbishang <774356294@qq.com>
2018-05-17 19:45:18 +0800

    LinkedListQueue

--- 03-Linked-List/LinkedListQueue.java
+++ 03-Linked-List/LinkedListQueue.java
@@ -1,6 +1,74 @@
 
 //基于链表的队列
-public class LinkedListQueue<E> {
+public class LinkedListQueue<E> implements Queue<E> {
+
+    private class Node{
+        private E e;
+        private Node next;
+
+        public Node(E e,Node next){
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e){
+            this.e = e;
+            this.next = null;
+        }
+
+        public Node(){
+            this.e = null;
+            this.next = null;
+        }
+
+        public String toString(){
+            return e.toString();
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
 
 
+    //构造函数
+    public LinkedListQueue(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public void enqueue(E e) {
+
+    }
+
+    @Override
+    public E getFront() {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    public void enququeFirst(E e){
+        Node node = new Node(e);
+        node.next = head.next;
+        head = node;
+
+
+    }
 }
--- Test/Test.java
+++ Test/Test.java
@@ -1,5 +1,23 @@
 public class Test {
     public static  void main(String[] args) {
+/*        LinkedList<Integer> linkedList = new LinkedList<>();
+        for(int i=0 ; i<6 ; i++){
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+        System.out.println(linkedList.remove(3));
+        System.out.println(linkedList.toString());*/
 
+        LinkedListStack linkedListStack = new LinkedListStack();
+        for(int i=0 ; i<5; i++){
+            linkedListStack.push(i);
+            System.out.println(linkedListStack);
+        }
+        System.out.println("此时栈的容量为"+linkedListStack.getSize());
+        System.out.println("栈头元素为："+linkedListStack.peek());
+        linkedListStack.pop();
+        System.out.println(linkedListStack);
     }
+
+
 }
